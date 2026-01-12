dc3ef012e3012037cba1bbf0c17ad0de1ef7c3a5
HuberttGit <hubert.talib@gmail.com>
2026-01-12 16:13:27 +0100

    update

---
+++ @mf-types/angularRemote/Style.d.ts
@@ -0,0 +1,2 @@
+export * from './compiled-types/app/ui/style';
+export { default } from './compiled-types/app/ui/style';
--- @mf-types/angularRemote/compiled-types/app/ui/login-ui/login-ui.entry.d.ts
+++ @mf-types/angularRemote/compiled-types/app/ui/login-ui/login-ui.entry.d.ts
@@ -1,4 +1,3 @@
 import 'zone.js';
-import '@styles/styles.scss';
 declare const mount: () => Promise<void>;
 export { mount };
