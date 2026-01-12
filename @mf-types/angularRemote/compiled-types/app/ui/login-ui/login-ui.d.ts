fd14ba75977427638f57e697e435abb1dfe27e51
HuberttGit <hubert.talib@gmail.com>
2026-01-12 08:24:09 +0100

    create new cmp

--- @mf-types/angularRemote/compiled-types/app/ui/login-ui/login-ui.d.ts
+++ @mf-types/angularRemote/compiled-types/app/ui/login-ui/login-ui.d.ts
@@ -1,6 +1,5 @@
 export declare class LoginUi {
     onLogin: import("@angular/core").OutputEmitterRef<"google" | "github">;
-    isLoading: import("@angular/core").InputSignal<boolean>;
     loginWithGithub(): void;
     loginWithGoogle(): void;
 }
